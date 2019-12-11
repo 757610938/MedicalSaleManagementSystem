049dcd892ef4a3d9249a21437f2c59431dd30183
757610938 <757610938@qq.com>
2019-12-12 00:08:40 +0800

    添加resful返回工具类

---
+++ src/main/java/com/medicalSaleManagementSystem/core/bean/DTO/UserDTO.java
@@ -0,0 +1,155 @@
+package com.medicalSaleManagementSystem.core.bean.DTO;
+
+import java.util.Date;
+
+public class UserDTO {
+    private Integer userId;
+
+    private String userName;
+
+    private String password;
+
+    private Integer valid;
+
+    private String lastLoginIp;
+
+    private Date lastLoginTime;
+
+    private Date genTime;
+
+    private String userSex;
+
+    private Date userBirthday;
+
+    private String userIdCard;
+
+    private String userAddress;
+
+    private String userPhone;
+
+    private String userEmail;
+
+    private String userNumber;
+
+    private String userPicture;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getValid() {
+        return valid;
+    }
+
+    public void setValid(Integer valid) {
+        this.valid = valid;
+    }
+
+    public String getLastLoginIp() {
+        return lastLoginIp;
+    }
+
+    public void setLastLoginIp(String lastLoginIp) {
+        this.lastLoginIp = lastLoginIp == null ? null : lastLoginIp.trim();
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public Date getGenTime() {
+        return genTime;
+    }
+
+    public void setGenTime(Date genTime) {
+        this.genTime = genTime;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex == null ? null : userSex.trim();
+    }
+
+    public Date getUserBirthday() {
+        return userBirthday;
+    }
+
+    public void setUserBirthday(Date userBirthday) {
+        this.userBirthday = userBirthday;
+    }
+
+    public String getUserIdCard() {
+        return userIdCard;
+    }
+
+    public void setUserIdCard(String userIdCard) {
+        this.userIdCard = userIdCard == null ? null : userIdCard.trim();
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress == null ? null : userAddress.trim();
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone == null ? null : userPhone.trim();
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail == null ? null : userEmail.trim();
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber == null ? null : userNumber.trim();
+    }
+
+    public String getUserPicture() {
+        return userPicture;
+    }
+
+    public void setUserPicture(String userPicture) {
+        this.userPicture = userPicture == null ? null : userPicture.trim();
+    }
+}
---
+++ src/test/java/com/medicalSaleManagementSystem/core/bean/DtoCopyToPojo.java
@@ -0,0 +1,21 @@
+//package com.medicalSaleManagementSystem.core.bean;
+//
+//import com.medicalSaleManagementSystem.core.bean.DTO.EmployeeDTO;
+//import com.medicalSaleManagementSystem.core.bean.VO.EmployeeVO;
+//import com.medicalSaleManagementSystem.util.BeanUtilEx;
+//import org.junit.Test;
+//
+//public class DtoCopyToPojo {
+//    @Test
+//    public void DtoCopyToPojo(){
+//        EmployeeDTO employeeDTO = new EmployeeDTO();
+//        employeeDTO.setEmpAccount("linxianqin");
+//        employeeDTO.setEmpPassword("12345678");
+//        EmployeeVO employeeVO = new EmployeeVO();
+//        System.out.println("转化前employeeDTO:"+employeeDTO);
+//        System.out.println("转化前employeeVO:"+employeeVO);
+//        BeanUtilEx.copyProperties(employeeVO,employeeDTO);
+//        System.out.println("转化后employeeDTO:"+employeeDTO);
+//        System.out.println("转化后employeeVO:"+employeeVO);
+//    }
+//}
