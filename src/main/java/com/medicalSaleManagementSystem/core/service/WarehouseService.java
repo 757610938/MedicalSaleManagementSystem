e6855f6f74a8898bceb599b4721ac717f47301f6
ChowKachun <49422291+ChowKachun@users.noreply.github.com>
2019-12-11 22:26:25 +0800

    增加了仓库业务

--- src/main/java/com/medicalSaleManagementSystem/core/service/WarehouseService.java
+++ src/main/java/com/medicalSaleManagementSystem/core/service/WarehouseService.java
@@ -1,4 +1,27 @@
 package com.medicalSaleManagementSystem.core.service;
 
+import com.medicalSaleManagementSystem.util.message.Msg;
+
 public interface WarehouseService {
+
+    /**
+     * 通过仓库Id查询仓库
+     * @param whseId
+     * @return
+     */
+    Msg findWhseByWhseId(Integer whseId);
+
+    /**
+     * 通过仓库名查找仓库
+     * @param whseName
+     * @return
+     */
+    Msg findWhseByWhseName(String whseName);
+
+    /**
+     * 通过仓库地址查找仓库
+     * @param whseAddress
+     * @return
+     */
+    Msg findWhseByWhseAddress(String whseAddress);
 }
